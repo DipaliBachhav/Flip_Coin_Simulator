e66f320bf0d6e6c44e3758b0ebdf8f8c7471cc41
DipaliBachhav <dipa2718@gmail.com>
2020-04-13 19:41:22 +0530

    Added Singlet Filp Coin Combination With Percent

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,20 @@
 #!/bin/bash -x
 echo "Flip Coin Simulator"
 
+declare -A dictionary
+for ((i=0; i<=10; i++ ))
+do
 coin=$((RANDOM%2))
 if [[ $coin -eq 1 ]]
 then
-	echo "Head"
+	((head++))
 else
-	echo "Tail"
+	((tail++))
 fi
+done
+dictionary[0]=$head
+dictionary[1]=$tail
+
+echo "${dictionary[@]}"
+per_Head=$(( ($head*100)/10 ))
+per_Tail=$(( ($tail*100)/10 ))
