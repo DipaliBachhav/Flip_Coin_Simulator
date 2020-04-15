75a7cd1a5ab2b25187e3c13294b3c45df4fb2a09
DipaliBachhav <dipa2718@gmail.com>
2020-04-15 09:31:38 +0530

    Added Doublet Flip Coin Combination With Percentage

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 echo "Flip Coin Simulator"
-
+#Singlet Combination
 declare -A dictionary
 for ((i=0; i<=10; i++ ))
 do
@@ -18,3 +18,43 @@ dictionary[1]=$tail
 echo "${dictionary[@]}"
 per_Head=$(( ($head*100)/10 ))
 per_Tail=$(( ($tail*100)/10 ))
+
+#Doublet Combination
+HH=0
+HT=0
+TT=0
+TH=0
+flipCoin=10
+count=0
+declare -A doublet
+while [ $count -lt $flipCoin ]
+do
+	coin1=$(( RANDOM%2 ))
+	coin2=$(( RANDOM%2 ))
+		if [[ $coin1 -eq 1 && $coin2 -eq 1 ]]
+		then
+			(( HH++ ))
+		elif [[ $coin1 -eq 1 && $coin2 -eq 0 ]]
+                then
+                        (( HT++ ))
+
+		elif [[ $coin1 -eq 0 && $coin2 -eq 1 ]]
+                then
+                        (( TH++ ))
+		else
+			(( TT++ ))
+		fi
+(( count++ ))
+done
+
+doublet[0]=$HH
+doublet[1]=$HT
+doublet[2]=$TH
+doublet[3]=$TT
+
+echo "${doublet[@]}"
+
+per_HH=$((($HH*100)/10))
+per_HT=$((($HT*100)/10))
+per_TH=$((($TH*100)/10))
+per_TT=$((($TT*100)/10))
