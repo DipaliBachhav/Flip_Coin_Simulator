ab411c3beef027c16896d4bcd99efd40ba8525e4
DipaliBachhav <dipa2718@gmail.com>
2020-04-16 11:06:24 +0530

    Refactor Using Function

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,138 +1,68 @@
 #!/bin/bash -x
 echo "Flip Coin Simulator"
-#Singlet Combination
+
 declare -A dictionary
-for ((i=0; i<=10; i++ ))
-do
-coin=$((RANDOM%2))
-if [[ $coin -eq 1 ]]
+HEAD=0
+TAIL=1
+random=$((RANDOM%2))
+if [ $random -eq $HEAD ]
 then
-	((head++))
+	echo "Head"
 else
-	((tail++))
+        echo "Tail"
 fi
-done
-dictionary[0]=$head
-dictionary[1]=$tail
 
-echo "${dictionary[@]}"
-per_Head=$(( ($head*100)/10 ))
-per_Tail=$(( ($tail*100)/10 ))
+function combination(){
+        for (( i=0; i<$1; i++ ))
+        do
+                coin=""
+      for (( j=0; j<$2; j++ ))
+      do
+         random=$((RANDOM%2))
+         if [ $random -eq $HEAD ]
+         then
+            coin+=H
+         else
+            coin+=T
+         fi
+                done
+      dictionary[$coin]=$(( ${dictionary[$coin]}+1 ))
+        done
+   echo "Combination : ${dictionary[@]}"
+   echo  "keys : ${!dictionary[@]}"
+}
 
-#Doublet Combination
-HH=0
-HT=0
-TT=0
-TH=0
-flipCoin=10
-count=0
-declare -A doublet
-while [ $count -lt $flipCoin ]
+
+function percentageFind(){
+   for key in ${!dictionary[@]}
+   {
+      percentage=$((${dictionary[$keys]}))/$flipCoin*100
+      echo  "$key =  $percentage"
+   }
+}
+read -p "Enter How Many Time Coin Flip := " flipCoin
+read -p "1)Singlet Combination 2)Doublet Combination 3)Triplet Combination " choice
+
+# Check choice
+if [ $choice -eq 1 ]
+then
+   combination $flipCoin $choice
+   percentageFind
+elif [ $choice -eq 2 ]
+then
+   combination $flipCoin $choice
+   percentageFind
+elif [ $choice  -eq 3 ]
+then
+   combination $flipCoin $choice
+   percentageFind
+else
+   echo "Invalid Choice..."
+fi
+
+# sort in desending order
+for key in ${!dictionary[@]}
 do
-	coin1=$(( RANDOM%2 ))
-	coin2=$(( RANDOM%2 ))
-		if [[ $coin1 -eq 1 && $coin2 -eq 1 ]]
-		then
-			(( HH++ ))
-		elif [[ $coin1 -eq 1 && $coin2 -eq 0 ]]
-                then
-                        (( HT++ ))
-
-		elif [[ $coin1 -eq 0 && $coin2 -eq 1 ]]
-                then
-                        (( TH++ ))
-		else
-			(( TT++ ))
-		fi
-(( count++ ))
+        echo -n " $key ${dictionary[$key]}"
 done
-
-doublet[0]=$HH
-doublet[1]=$HT
-doublet[2]=$TH
-doublet[3]=$TT
-
-echo "${doublet[@]}"
-
-per_HH=$((($HH*100)/10))
-per_HT=$((($HT*100)/10))
-per_TH=$((($TH*100)/10))
-per_TT=$((($TT*100)/10))
-
-#Triplet Combination
-HHH=0
-HHT=0
-THH=0
-HTH=0
-THT=0
-TTT=0
-TTH=0
-HTT=0
-flipCoin=10
-count=0
-declare -A triplet
-while [ $count -lt $flipCoin ]
-do
-        coin1=$(( RANDOM%2 ))
-        coin2=$(( RANDOM%2 ))
-	coin3=$(( RANDOM%2 ))
-
-                if [[ $coin1 -eq 1 && $coin2 -eq 1 && $coin3 -eq 1 ]]
-                then
-                        (( HHH++ ))
-
-                elif [[ $coin1 -eq 1 && $coin2 -eq 1 && $coin3 -eq 0 ]]
-		then
-                        (( HHT++ ))
-
-                elif [[ $coin1 -eq 1 && $coin2 -eq 0 && $coin3 -eq 1 ]]
-                then
-                        (( HTH++ ))
-
-		elif [[ $coin1 -eq 0 && $coin2 -eq 1 && $coin3 -eq 1 ]]
-                then
-                        (( THH++ ))
-
-    		elif [[ $coin1 -eq 0 && $coin2 -eq 1 && $coin3 -eq 0 ]]
-                then
-                        (( THT++ ))
-
-    		elif [[ $coin1 -eq 1 && $coin2 -eq 0 && $coin3 -eq 0 ]]
-                then
-                        (( HTT++ ))
-
-		elif [[ $coin1 -eq 0 && $coin2 -eq 0 && $coin3 -eq 1 ]]
-                then
-                        (( TTH++ ))
-
-                else
-                        (( TTT++ ))
-                fi
-(( count++ ))
-done
-triplet[0]=$HHH
-triplet[1]=$HHT
-triplet[2]=$HTH
-triplet[3]=$THH
-triplet[4]=$THT
-triplet[5]=$TTT
-triplet[6]=$TTH
-triplet[7]=$HTT
-
-echo "Keys := ${!triplet[@]}" " Values := ${triplet[@]}"
-
-per_HHH=$((($HHH*100)/10))
-per_HHT=$((($HHT*100)/10))
-per_HTH=$((($HTH*100)/10))
-per_THH=$((($THH*100)/10))
-per_THT=$((($THT*100)/10))
-per_TTT=$((($TTT*100)/10))
-per_TTH=$((($TTH*100)/10))
-per_HTT=$((($HTT*100)/10))
-
-echo "Singlet Sorting :="
-sort -n <(printf "%s\n" "${dictionary[@]}")
-echo "Doublet Sorting :="
-sort -n <(printf "%s\n" "${doublet[@]}")
-echo "Triplet Sorting :="
-sort -n <(printf "%s\n" "${triplet[@]}")
+sort -n <(printf "%s\n" "${dictionary[$coin]}")
